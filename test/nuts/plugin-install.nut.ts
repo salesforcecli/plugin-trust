b3927ecf36f6a79d1fbf60387cd31699a3cf1245
mshanemc <shane.mclaughlin@salesforce.com>
2022-11-28 14:41:36 -0600

    test: assert that allowlist was written as expected

--- test/nuts/plugin-install.nut.ts
+++ test/nuts/plugin-install.nut.ts
@@ -6,7 +6,7 @@
  */
 
 import * as path from 'path';
-import * as fs from 'fs/promises';
+import * as fs from 'fs';
 import { expect } from 'chai';
 import { TestSession, execCmd, execInteractiveCmd, Interaction } from '@salesforce/cli-plugins-testkit';
 
@@ -18,16 +18,16 @@ describe('plugins:install commands', () => {
 
   before(async () => {
     session = await TestSession.create({ devhubAuthStrategy: 'NONE' });
-    await fs.mkdir(path.join(session.homeDir, '.sfdx'), { recursive: true });
+    await fs.promises.mkdir(path.join(session.homeDir, '.sfdx'), { recursive: true });
 
     const fileData: string = JSON.stringify({ acknowledged: true }, null, 2);
-    await fs.writeFile(path.join(session.homeDir, '.sfdx', 'acknowledgedUsageCollection.json'), fileData);
+    await fs.promises.writeFile(path.join(session.homeDir, '.sfdx', 'acknowledgedUsageCollection.json'), fileData);
 
     const configDir = path.join(session.homeDir, '.config', 'sfdx');
-    await fs.mkdir(configDir, { recursive: true });
+    await fs.promises.mkdir(configDir, { recursive: true });
 
     const unsignedMod: string = JSON.stringify([UNSIGNED_MODULE_NAME2], null, 2);
-    await fs.writeFile(path.join(configDir, 'unsignedPluginAllowList.json'), unsignedMod);
+    await fs.promises.writeFile(path.join(configDir, 'unsignedPluginAllowList.json'), unsignedMod);
 
     // ensure that this repo's version of the plugin is used and NOT the one that shipped with the CLI
     execCmd('plugins:link .', {
@@ -83,7 +83,8 @@ describe('plugins:install commands', () => {
     expect(result.stdout).to.contain('Finished digital signature check');
   });
 
-  it.skip('plugins:install unsigned plugin in the allow list', () => {
+  it('plugins:install unsigned plugin in the allow list', () => {
+    expect(fs.existsSync(path.join(session.homeDir, '.config', 'sfdx'))).to.be.true;
     const result = execCmd(`plugins:install ${UNSIGNED_MODULE_NAME2}`, {
       ensureExitCode: 0,
       cli: 'sfdx',
