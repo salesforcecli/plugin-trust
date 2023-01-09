b762e08e188099b3f168ca57d4e14f759b10577a
Mike Donnalley <mdonnalley@salesforce.com>
2023-01-09 10:59:40 -0700

    feat: add JIT plugin install hook

---
+++ src/hooks/jitPluginInstall.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import * as os from 'os';
+import { Hook } from '@oclif/core';
+import { SfError } from '@salesforce/core';
+import { TelemetryGlobal } from '@salesforce/plugin-telemetry/lib/telemetryGlobal';
+import { AppInsights } from '@salesforce/telemetry/lib/appInsights';
+
+declare const global: TelemetryGlobal;
+
+const hook: Hook<'jit_plugin_not_installed'> = async function (opts) {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    global.cliTelemetry?.record({
+      eventName: 'JIT_INSTALL_STARTED',
+      type: 'EVENT',
+      version: opts.config.version,
+      plugin: opts.command.pluginName,
+      command: opts.command.id,
+    });
+
+    await opts.config.runCommand('plugins:install', [`${opts.command.pluginName}@${opts.pluginVersion}`]);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    global.cliTelemetry?.record({
+      eventName: 'JIT_INSTALL_SUCCESS',
+      type: 'EVENT',
+      version: opts.config.version,
+      plugin: opts.command.pluginName,
+      command: opts.command.id,
+    });
+  } catch (error) {
+    const err = error as Error;
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    global.cliTelemetry?.record({
+      eventName: 'JIT_INSTALL_FAILED',
+      type: 'EVENT',
+      message: err.message,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+      stackTrace: err?.stack?.replace(new RegExp(os.homedir(), 'g'), AppInsights.GDPR_HIDDEN),
+      version: opts.config.version,
+      plugin: opts.command.pluginName,
+      command: opts.command.id,
+    });
+
+    throw new SfError(`Could not install ${opts.command.pluginName}`, 'JitPluginInstallError');
+  }
+};
+
+export default hook;
